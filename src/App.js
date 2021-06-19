219c6d165c4d2cde9f4c472cce7b0d3f5b9894a8
John Kim <jkim005@yahoo.com>
2021-06-18 18:42:27 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Products from './components/Products.js'
+
+function App() {
+  return (
+    <div className="App">
+        <p>
+          Hello World
+        </p>
+        <Products/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Products = () => {
+    return (
+        <div>
+            Products here
+        </div>
+    )
+}
+
+export default Products
